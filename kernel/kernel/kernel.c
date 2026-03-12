f2d02f4bcc3335b5226c7f9f97c4fddbe1daf1c7
Yiyoung Liu <yiyoung.md@gmail.com>
2026-03-12 11:44:33 -0400

    fix: compiler error and removed some preprocessing

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -2,17 +2,6 @@
 
 #include <kernel/tty.h>
 
-/* Check if the compiler thinks you are targeting the wrong OS. */
-#if defined(__linux__)
-#error "You are not using a cross-compiler, \
-        certainly this is not what you want to do"
-#endif
-
-/* Check that the target is 32-bit ix86 */
-#if !defined(__i386__)
-#error "This needs to be compiled with a ix86-elf compiler"
-#endif
-
 void kernel_main(void)
 {
   /* initalize terminal interface*/
