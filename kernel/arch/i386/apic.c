994741338a4ba5f459602bc1225fca13ecb96d40
Yiyoung Liu <yiyoung.md@gmail.com>
2026-04-02 21:58:21 -0400

    boot(refactor): remove some magic numbers and add some comments

--- kernel/arch/i386/apic.c
+++ kernel/arch/i386/apic.c
@@ -3,7 +3,8 @@
 #include <stdint.h>
 
 #define CPUID_FEAT_EDX_APIC 1 << 9
-#define IA32_APIC_BASE 0x1B
+#define APIC_REGISTER_ADDRESS 0x1B
+#define APIC_BASE_MASK 0xFFFFF000
 
 bool check_apic() {
 	uint32_t eax, edx;
@@ -14,13 +15,13 @@ bool check_apic() {
 void enable_local_apic() {
 	uint32_t eax, edx;
 	// find apic msr location
-	__asm__ __volatile__ ("rdmsr" : "=a" (eax), "=d" (edx) : "c" (IA32_APIC_BASE));
+	__asm__ __volatile__ ("rdmsr" : "=a" (eax), "=d" (edx) : "c" (APIC_REGISTER_ADDRESS));
 
 	// no paging implemented yet
-	uint32_t base = eax & 0xfffff000;
+	uint32_t *base = (uint32_t*) (eax & APIC_BASE_MASK);
 
 	// enable apic, map spurious interrupt vector register
-	__asm__ __volatile__ ("mov %0, (%1)" : : "r" (0x1FF), "r" (base | 0xF0));
+	*(base | 0xF0) = 0x1FF;
 }
 
 void disable_pic() {
@@ -29,9 +30,11 @@ void disable_pic() {
 }
 
 void enable_apic() {
+	// should probably do something with this
 	check_apic();
 
 	enable_local_apic();
 
+	// need to figure out what this actually does
 	disable_pic();
 }
