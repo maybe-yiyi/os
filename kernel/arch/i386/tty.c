df8f039dd41260140f9c8ab5834d554187e1cd4a
Yiyoung Liu <yiyoung.md@gmail.com>
2026-04-30 21:26:12 -0400

    feat(tty): make putchar dumber so it wraps around always

--- kernel/arch/i386/tty.c
+++ kernel/arch/i386/tty.c
@@ -86,11 +86,15 @@ void terminal_putchar(char c)
 		terminal_column = 0;
 		increment_row();
 	} else if (c == '\b') {
-		if (terminal_column != 0) {
-			terminal_column--;
-			terminal_putentryat(' ', terminal_color,
-					    terminal_column, terminal_row);
+		if (terminal_column == 0) {
+			if (terminal_row == 0)
+				return;
+			terminal_column = VGA_WIDTH;
+			terminal_row--;
 		}
+		terminal_column--;
+		terminal_putentryat(' ', terminal_color, terminal_column,
+				    terminal_row);
 	} else if (c == '\t') {
 	} else {
 		terminal_putentryat(c, terminal_color, terminal_column,
